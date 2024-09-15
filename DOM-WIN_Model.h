c41b17e09cbde1566c316dd171832ea16f980073
Zanatta <121562999+Zommodore@users.noreply.github.com>
2024-09-15 13:51:20 -0300

    Update DOM-WIN_Model.h

--- DOM-WIN_Model.h
+++ DOM-WIN_Model.h
@@ -1,9 +1,3 @@
-//ProjetoDOMINO - Projeto  Domino (model.h)
-//Carlos Gabriel Gouveia
-//Diogo Casari del Pino
-//Gustavo Nascimento Zanatta
-//Juan Fernando Coucolis Isano
-
 int mesaE,mesaD;
 int tamanho;
 char jvez;
