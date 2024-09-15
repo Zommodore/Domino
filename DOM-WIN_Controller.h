f49ebe403a03c6addf6c317422857025dd7f9ca6
Zanatta <121562999+Zommodore@users.noreply.github.com>
2024-09-15 13:34:36 -0300

    Create DOM-WIN_Controller.h

---
+++ DOM-WIN_Controller.h
@@ -0,0 +1,22 @@
+//ProjetoDOMINO - Projeto  Domino (controller.h)
+//Carlos Gabriel Gouveia
+//Diogo Casari del Pino
+//Gustavo Nascimento Zanatta
+//Juan Fernando Coucolis Isano
+void fExecutarMenu();//executa o menu 
+void fMenuJogo();//menu do jogo
+void fEmbaralhar();//funcao para embaralhar as pecas e entregar aos jogadores
+void fDistribuirPecas();//contador das pecas para ambos jogadores
+void fPrimeiraJogada();// funcao para jogar a primeira peca na mesa
+void fTrocaJogador();//funcao para trocar o jogador
+void fJogar(int indice,char lado);//funcao para jogar
+void fClear();// limpa tela
+booleano fPassar();//funcao para passar a vez
+booleano fDepositoVazio();//indentifica se ha pecas no deposito para comprar
+void fComprar();//funcao para comprar pecas
+int fDesempate();//serve para indentificar o vencedor 
+void fGravaCadastro();//funcao que salva a partida
+void fRecuperaCadastro();//funcao que recupera partida anteriormente salva
+void fComputador(); //funcao de jogar contra o computador
+void fMenuJogoComputador();//menu do jogo contra o computador
+int fJogadorNaoTemPecas(int jogador);//funcao para quando jogador ficar sem peca
