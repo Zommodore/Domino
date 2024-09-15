0203a5c5de4945e61be762bba558dbd4e1aede29
Zanatta <121562999+Zommodore@users.noreply.github.com>
2024-09-15 13:50:25 -0300

    Update DOM-WIN-Projeto.c

--- DOM-WIN-Projeto.c
+++ DOM-WIN-Projeto.c
@@ -1,9 +1,3 @@
-//ProjetoDOMINO - Projeto  Domino (main)
-//Carlos Gabriel Gouveia
-//Diogo Casari del Pino
-//Gustavo Nascimento Zanatta
-//Juan Fernando Coucolis Isano
-
 #include "DOM-WIN_Model.cpp"
 #include"DOM-WIN_Controller.cpp"
 
