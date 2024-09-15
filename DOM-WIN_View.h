f29168c5b89ca1813efd15da81d7cbef6f54b73a
Zanatta <121562999+Zommodore@users.noreply.github.com>
2024-09-15 13:38:38 -0300

    Create DOM-WIN_View.h

---
+++ DOM-WIN_View.h
@@ -0,0 +1,14 @@
+//ProjetoDOMINO - Projeto  Domino (view.h)
+//Carlos Gabriel Gouveia
+//Diogo Casari del Pino
+//Gustavo Nascimento Zanatta
+//Juan Fernando Coucolis Isano
+int fMenuGeral();//menu inicial do jogo
+int fMenuDois();//menu para jogos de dois jogaodres
+int fMenuComputador(); //menu do jogo contra o computador
+int compcontra; 
+int multiplayer; 
+void fApresentaMesa();//funcao para apresentar mesa 
+void fApresentaPeca();//funcao para mostrar as pecas de tal jogador
+void fJogoContraDois();//funcao para jogos entre dois jogadores
+void fApresentaMensagem(char mens[100]);
