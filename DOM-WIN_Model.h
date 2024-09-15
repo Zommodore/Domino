c0dd927f5019b83c5ae2feb8e6b1ef8e96ca574f
Zanatta <121562999+Zommodore@users.noreply.github.com>
2024-09-15 13:39:18 -0300

    Create DOM-WIN_Model.h

---
+++ DOM-WIN_Model.h
@@ -0,0 +1,13 @@
+//ProjetoDOMINO - Projeto  Domino (model.h)
+//Carlos Gabriel Gouveia
+//Diogo Casari del Pino
+//Gustavo Nascimento Zanatta
+//Juan Fernando Coucolis Isano
+
+int mesaE,mesaD;
+int tamanho;
+char jvez;
+int jogcomp;
+typedef int booleano;
+#define FALSE 0
+#define TRUE 1
